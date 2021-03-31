1fc85d4099a568b8c6d06fd26651120f13ba9d22
sinbox0701 <psh090953@gmail.com>
2021-04-01 00:39:56 +0900

    cors

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ const apollo = new ApolloServer({
 const app = express();
 app.use(logger("tiny"));
 app.use("/static",express.static("uploads"));
-apollo.applyMiddleware({app});
+apollo.applyMiddleware({app,cors:false});
 app.listen({port:PORT}, () => {
     console.log(`Server is running on http://localhost:${PORT}/graphql`)
 });
