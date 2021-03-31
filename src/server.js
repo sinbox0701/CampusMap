47ee8b5f14ea93f98f23c8ee1bcc0f989db79559
sinbox0701 <psh090953@gmail.com>
2021-03-31 13:57:28 +0900

    introspection

--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ const apollo = new ApolloServer({
     typeDefs,
     resolvers,
     playground:true,
+    introspection:true,
     context: async({req}) => {
         return {
             loggedInUser: await getUser(req.headers.authorization),
