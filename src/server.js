dc95a8e843dec4a00bd45846119c9f4b068615b2
sinbox0701 <psh090953@gmail.com>
2021-04-16 20:17:09 +0900

    test mode

--- src/server.js
+++ src/server.js
@@ -11,6 +11,8 @@ const PORT = process.env.PORT;
 const apollo = new ApolloServer({
     typeDefs,
     resolvers,
+    playground:true,
+    introspection:true,
     context: async({req}) => {
         return {
             loggedInUser: await getUser(req.headers.authorization),
